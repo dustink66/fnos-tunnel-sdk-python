d973c43991e465e081aeba26aa7cf91082399dda
dustink <dustink@qq.com>
2026-06-14 14:09:58 +0800

    Bump to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fnos-tunnel",
-    version="1.0.0",
+    version="1.0.1",
     description="fnOS Cloudflare Tunnel SDK for Python",
     author="fnOS",
     packages=find_packages(),
